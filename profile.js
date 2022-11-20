59cd0bd85b8ce4147b75ae88c8f2766717e4b550
Robert Culbert <rfculbert@gmail.com>
2022-11-20 13:04:31 -0600

    thrid commit

--- profile.js
+++ profile.js
@@ -1,5 +1,14 @@
-button.addEventListener('click', () =>{
-    alert()
-})
+// button.addEventListener('click', () =>{
+//     alert('My favorite Color is Blue')
+// })
 
-const button = document.querySelector('')
+// const button = document.querySelector('#color')
+
+const colorBtn = document.querySelector('color')
+
+function showColor () {
+    alert('Blue is my favorite color.')
+}
+
+
+colorBtn.addEventListener('click', showColor)
